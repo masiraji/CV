265971cdaa245904e428ec7630102f29f5a61d21
masiraji <siraji1993@gmail.com>
2022-01-11 21:26:32 +0800

    github pages

--- render_cv.r
+++ render_cv.r
@@ -8,7 +8,7 @@
 # Knit the HTML version
 rmarkdown::render("cv.rmd",
                   params = list(pdf_mode = FALSE),
-                  output_file = "docs/cv.html")
+                  output_file = "docs/index.html")
 
 # Knit the PDF version to temporary html location
 tmp_html_cv_loc <- fs::file_temp(ext = ".html")
