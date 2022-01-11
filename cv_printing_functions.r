355f2203be2a12a258464bc0e499c315890ca4e8
masiraji <siraji1993@gmail.com>
2022-01-12 00:50:05 +0800

    Updates with references

--- cv_printing_functions.r
+++ cv_printing_functions.r
@@ -40,18 +40,21 @@ create_CV_object <-  function(data_location,
     read_gsheet <- function(sheet_id){
       googlesheets4::read_sheet(data_location, sheet = sheet_id, skip = 1, col_types = "c")
     }
-    cv$entries_data  <- read_gsheet(sheet_id = "entries")
+    cv$entries_data  <- read_gsheet(sheet_id = "entries") %>% 
+      filter(in_resume)
+    
     cv$skills        <- read_gsheet(sheet_id = "language_skills")
     cv$text_blocks   <- read_gsheet(sheet_id = "text_blocks")
     cv$contact_info  <- read_gsheet(sheet_id = "contact_info")
   } else {
     # Want to go old-school with csvs?
-    cv$entries_data <- readr::read_csv(paste0(data_location, "entries.csv"), skip = 1)
-    cv$skills       <- readr::read_csv(paste0(data_location, "language_skills.csv"), skip = 1)
+    cv$entries_data <- readr::read_csv(paste0(data_location, "entries.csv"), skip = 1) 
+        cv$skills       <- readr::read_csv(paste0(data_location, "language_skills.csv"), skip = 1)
     cv$text_blocks  <- readr::read_csv(paste0(data_location, "text_blocks.csv"), skip = 1)
     cv$contact_info <- readr::read_csv(paste0(data_location, "contact_info.csv"), skip = 1)
   }
-
+  cv$entries_data <-  cv$entries_data%>% 
+    subset(in_resume=="TRUE")
 
   extract_year <- function(dates){
     date_year <- stringr::str_extract(dates, "(20|19)[0-9]{2}")
